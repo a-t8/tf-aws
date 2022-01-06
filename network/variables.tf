6556ace8aad4e49375b5b216185537c6898fcdf3
a-t8 <atulongit@gmail.com>
2022-01-06 20:17:06 +0530

    formatted terraform files

--- network/variables.tf
+++ network/variables.tf
@@ -1,20 +1,20 @@
 # -- network/variables.tf 
 
 variable "vpc_cidr" {
-    type = string
+  type = string
 }
 
 variable "public_cidrs" {
-    type = list
+  type = list(any)
 }
 
 variable "private_cidrs" {
-    type = list
-  
+  type = list(any)
+
 }
 variable "public_sn_count" {
-    type = number
-  
+  type = number
+
 }
 
 variable "private_sn_count" {
