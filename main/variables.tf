501487e0d3cd5372cdc89c02c604279c46197f24
Vladimir Mukhin <Vladimir_Mukhin@epam.com>
2022-01-15 23:31:32 -0500

    Remove aws_region var

--- main/variables.tf
+++ main/variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "The AWS Region of Deployment"
-  type        = string
-  default     = "us-west-2"
-}
-
 variable "env_code" {
   description = "Value of the enviroment like dev or prod"
   type        = string
